b7f7d5b4ccc71cf3722bbe70c84da7f4e7d9edc6
Chernyaev <chernyaev.anton.a@gmail.com>
2016-01-02 00:21:08 +0200

    'Testing'

---
+++ src/lesson1/HashTable.java
@@ -0,0 +1,110 @@
+package lesson1;
+
+public class HashTable {
+
+	private String ar[][] = new String[100][2];
+
+	HashTable() {
+		for (int i = 0; i < 100; i++) {
+			ar[i][0] = "-1";
+		}
+	}
+
+	public void add(String[] initialValues) {
+		for (String initValue : initialValues) {
+			byte hash_code = hash(initValue);
+			byte index = (byte) (hash_code & (ar.length - 1));
+			if (ar[index][0].equals("-1")) {
+				ar = set(index, hash_code, initValue, ar);
+			} else {
+				System.out.println("Collision. Position " + index
+						+ " is not empty. Try to find new place");
+				byte colPosition = (byte) (index + 1);
+				while (true) {
+					if (colPosition < ar.length) {
+						if (ar[colPosition][0].equals("-1")) {
+							ar = set(colPosition, hash_code, initValue, ar);
+							break;
+						}
+					}
+					colPosition++;
+					if (colPosition > ar.length) {
+						System.out.println("Not empty cell for this value");
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	public void remove(String value) {
+		byte hash_code = hash(value);
+		byte index = (byte) (hash_code & (ar.length - 1));
+		while (true) {
+			if (index < ar.length) {
+				if (ar[index][0].equals(hash_code + "")) {
+					System.out.println("Value " + value + " removed from position " + index);
+					ar[index][0] = "-1";
+					ar[index][1] = "";
+					break;
+				}
+			}
+			index++;
+			if (index > ar.length) {
+				System.out
+						.println("Value " + value + " not fouded in hash-table. Can't remove value.");
+				break;
+			}
+		}
+	}
+
+	public void find(String value) {
+		byte hash_code = hash("Moby Dick");
+		byte index = (byte) (hash_code & (ar.length - 1));
+		while (true) {
+			if (index < ar.length) {
+				if (ar[index][0].equals(hash_code + "")) {
+					System.out.println("Value " + value + " founded in position " + index);
+					break;
+				} else {
+					System.out
+							.println("Value " + value + " not founded in position " + index);
+				}
+			}
+			index++;
+			if (index > ar.length) {
+				System.out
+						.println("Value " + value + " not fouded in hash-table. Searching stopped.");
+				break;
+			}
+		}
+
+	}
+
+	private byte hash(String str) {
+		byte bb[] = str.getBytes();
+		byte hash_code = 1;
+		for (byte b : bb) {
+			hash_code *= b;
+			if (hash_code == 0)
+				hash_code++;
+		}
+
+		if (hash_code < 0)
+			hash_code += 128;
+
+		return hash_code;
+
+	}
+
+	private String[][] set(byte index, byte hash_code, String initValue,
+			String ar[][]) {
+		System.out.println("Position " + index + " is empty. Added "
+				+ initValue + " to position " + index);
+		ar[index][0] = hash_code + "";
+		ar[index][1] = initValue;
+		return ar;
+
+	}
+
+}
---
+++ src/lesson1/Main.java
@@ -0,0 +1,17 @@
+package lesson1;
+
+public class Main{
+	
+	public static void main(String[] args) {
+		HashTable table = new HashTable();
+		
+		String[] initialValues = new String[] { "Moby Dick", "James Huges",
+				"Alan Cooper", "Freddy Mercury", "yboM Dick" };
+		
+		table.add(initialValues);
+		table.remove("Moby Dick");
+		table.find("yboM Dick");
+		
+	}
+
+}
